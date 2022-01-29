30ca7200e1a22ce4e3e9c05df06f1a9c3a4bba7d
xendv <dvoynina.ksusha@mail.ru>
2022-01-29 17:07:55 +0300

    deleted useless files

--- src/scripts/upload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('./classes/dataBaseManager.php');
-require_once(__DIR__.'\classes\dataUploader.php');
-//if ()
-$dataUploader = new dataUploader;
-
-$dataUploader->connect();
-
